9962629855a53f9ea1f226796b887823585a0b6b
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-10-14 20:10:33 +0200

    Finished 0.1

--- main.sh
+++ main.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 toolsDir='/tools'
 DOMAIN="$1"
-baseDir="/root/$DOMAIN"
-
+data_dir="/data"
 function notify {
     python3 /code/message.py "$1"
 }
 
-httpx -silent -no-color -l "$baseDir/domains-$DOMAIN.txt" -title -content-length -web-server -status-code -ports 80,443 -threads 25 -o "$baseDir/httpx-$DOMAIN.txt"
+function httpx-exec {
+    httpx -silent -no-color -l "$data_dir/final.txt" -title -content-length -web-server -status-code -ports 80,443 -threads 25 -o "$data_dir/httpx-$DOMAIN.txt"
+}
 
+function main {
+    httpx-exec
+}
 
-notify "$1"
-
+{ time main ; } 2> time.txt
+t=$(cat time.txt | grep real | cut -d " " -f2)
+c=$(cat /data/httpx-$DOMAIN.txt | wc -l)
+realistic=$(($c / 2))
+notify "HTTPX found $c ($realistic) online for $DOMAIN in $t"
